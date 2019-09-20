bbec0ff68dcdb017687a6e79e0d3d640fcabca15
Monil Shah <monilhshah@gmail.com>
2019-09-20 09:48:34 +0000

    Added hostname to distinguish container

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
 
 @SpringBootApplication
 @RestController
@@ -11,7 +12,8 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        String hostName = InetAddress.getLocalHost().getHostName();
+        return "Hello from " + hostName;
     }
 
     public static void main(String[] args) {
