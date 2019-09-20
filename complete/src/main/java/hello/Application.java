87bf39c5d6ff3cf9990cbfeeaba450025b7ee1de
Monil Shah <monilhshah@gmail.com>
2019-09-20 11:33:50 +0000

    Fixed compilation issues

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -5,13 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
     @RequestMapping("/")
-    public String home() {
+    public String home() throws UnknownHostException{
         String hostName = InetAddress.getLocalHost().getHostName();
         return "Hello from " + hostName;
     }
